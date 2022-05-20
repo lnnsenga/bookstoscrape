dcaa83b07c0ad5898e9a4224d50e5c9b01ae0217
Leticia Nkulu <leticia.nkulu@gmail.com>
2022-05-20 15:05:38 +0300

    removed beautiful-soup.py

--- price-extractor.py
+++ price-extractor.py
@@ -1,4 +1,4 @@
-import pandas as pd
+import pandas as pdgit
 import requests
 from bs4 import BeautifulSoup
 
@@ -11,12 +11,13 @@ soup = BeautifulSoup(page.content, 'html.parser')
 week = soup.find(id='seven-day-forecast-body')
 
 items = week.find_all(class_='tombstone-container')
-# print(items[0].find(class_='period-name').get_text())  
-# print(items[0].find(class_='short-desc').get_text())  
-# print(items[0].find(class_='temp').get_text())  
+# print(items[0].find(class_='period-name').get_text())
+# print(items[0].find(class_='short-desc').get_text())
+# print(items[0].find(class_='temp').get_text())
 
 period_names = [item.find(class_='period-name').get_text() for item in items]
-short_descriptions = [item.find(class_='short-desc').get_text() for item in items]
+short_descriptions = [
+    item.find(class_='short-desc').get_text() for item in items]
 temperatures = [item.find(class_='temp').get_text() for item in items]
 print(period_names)
 print(short_descriptions)
@@ -25,9 +26,9 @@ print(temperatures)
 
 weather_stuff = pd.DataFrame(
     {
-        'period':period_names,
-        'short_descriptions':short_descriptions,
-        'temperatures':temperatures,
+        'period': period_names,
+        'short_descriptions': short_descriptions,
+        'temperatures': temperatures,
     }
 )
 
