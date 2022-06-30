f051b61dabb6575f9452ddd90efec9ab5f0e2b0e
lnnsenga <codingbaws@gmail.com>
2022-06-30 13:34:24 +0300

    removed unrelevant comments from all files and added a function to save images

--- utils.py
+++ utils.py
@@ -1,5 +1,6 @@
 import os
 import pandas as pd
+from PIL import Image
 import requests
 
 
@@ -14,13 +15,14 @@ def request_image(url):
 def save_to_csv(data, filename):
 
     print(filename)
-    product_info = pd.DataFrame(data=data)
+    product_info = pd.DataFrame(data)
+    f_name = filename.strip()
 
     # saves data to csv
     if not os.path.exists("csv_folder"):
             os.mkdir("csv_folder")
-    file_name = f"csv_folder/{filename.strip()}_category_products.csv"
-    product_info.to_csv(file_name)
+    file_name = f"csv_folder/{f_name}_category_products.csv"
+    product_info.to_csv(file_name,index=False)
 
 
 def save_book_to_csv(data, filename):
@@ -32,3 +34,15 @@ def save_book_to_csv(data, filename):
             os.mkdir("csv_folder")
     file_name = f"csv_folder/{filename.strip()}_info.csv"
     product_info.to_csv(file_name)
+
+
+def save_image(image_url,title,category_name):
+    img = Image.open(request_image(image_url))
+    if not os.path.exists(f"image_folder/{category_name}"):
+        os.mkdir(f"image_folder/{category_name}")
+
+    img.save(f"image_folder/{category_name}/{title}_book_image.jpg")
+
+
+
+# save_to_csv({'first name':'Lety','last name':'nk' }, "testing")
