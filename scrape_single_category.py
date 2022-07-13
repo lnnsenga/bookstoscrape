d27dc5c2cd88364002c9dcc708d1d7bb33e1040e
lnnsenga <codingbaws@gmail.com>
2022-07-13 12:02:38 +0300

    removed duplicate code for  scrape single  product,  re-used from  scrape_single_product.py

--- scrape_single_category.py
+++ scrape_single_category.py
@@ -1,6 +1,5 @@
-import pandas as pd
+from scrape_single_product import scrape_single_product
 from utils import request_page,save_image
-from PIL import Image
 from bs4 import BeautifulSoup
 
 
@@ -9,8 +8,6 @@ def scrape_single_category(url):
 
     data = []
     all_products_urls = []
-
-    product_info = {}
     one_page = request_page(url)
 
     soup = BeautifulSoup(one_page.content, 'html.parser')
@@ -30,66 +27,14 @@ def scrape_single_category(url):
         complete_url = single_product_url.replace(
             "../../..", "http://books.toscrape.com/catalogue")
 
-        # gets the product page
-        product_page = request_page(complete_url)
-        
-
-        product = BeautifulSoup(product_page.content, 'html.parser')
-
-        title = product.find('h1').get_text()
-        category_name = product.find(class_='breadcrumb').find_all('li')
+        single_product = scrape_single_product(complete_url)
       
+        image_url = single_product[0]['image_url']
+        title = single_product[0]['title']
+        category = single_product[0]['category']
 
-        if title.find("/"):
-            title = title.replace("/", "-")
-       
-
-        image = product.find('img')
-        image_url = image['src'].replace(
-            "../../", "http://books.toscrape.com/")
-
-        rating = product.find(class_='star-rating')
-        review_rating = rating['class']
-        review_rating.remove("star-rating")
-        review_rating= review_rating[0]
-      
-
-        cname = category_name[2].get_text() 
-        cname = cname.strip()
-
-        product_description = product.find(
-            id='product_description')
-
-        try:
-            product_description = product_description.find_next('p').get_text()
-        except AttributeError as error:
-            product_description = '--'
-        
-        product_information = product.find_all('td')
+        save_image(image_url,title,category)
+        data.append(single_product[0])
 
 
-        universal_product_code = product_information[0].get_text()
-        price_excluding_tax = product_information[2].get_text()
-        price_including_tax = product_information[3].get_text()
-        number_available = product_information[5].get_text()
-        number_available = number_available[10:12]
-    
-        save_image(image_url,title,cname)
-        data.append({
-        
-            'title':  str(title),
-            'universal_prod_code': str(universal_product_code),
-            'price_exc_tax': str(price_excluding_tax),
-            'price_inc_tax': str(price_including_tax),
-            'no_available': str(number_available),
-            'image_url': str(image_url),
-            'category': str(cname),
-            'r_rating': str(review_rating),
-            'prod_page_url': str(complete_url),
-            'prod_description': str(product_description), 
-        })
-
-    pd.set_option('display.max_colwidth', 20)
-    product_info = pd.DataFrame(data=data)
-
-    return product_info
+    return data
