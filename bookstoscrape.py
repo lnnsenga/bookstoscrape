23198b7740017f29659ec4132f42863dbefc246f
lnnsenga <codingbaws@gmail.com>
2022-07-13 11:58:28 +0300

    added a function to validate the URL entered by the user

--- bookstoscrape.py
+++ bookstoscrape.py
@@ -1,10 +1,9 @@
-import pandas as pd
+import validators
 from scrape_single_product import scrape_single_product
 from scrape_category_page import scrape_category_page
 from scrape_all_categories_books import scrape_all_categories_books
 from utils import save_book_to_csv
-from PIL import Image
-from bs4 import BeautifulSoup
+
 
 
 menu_options = {
@@ -22,16 +21,24 @@ def print_menu():
 
 def single_book():
     book_url = input("Enter the book URL:")
-    book_info = scrape_single_product(book_url)
-    save_book_to_csv(book_info, "single_book")
-    print('Book info has been saved!\n\n')
+    if (validators.url(book_url)):
+        book_info = scrape_single_product(book_url)
+        book = book_info[0]  
+        save_book_to_csv(book_info, f"single_book_{book['title']}")
+        print('Book info has been saved!\n\n')
+    else:
+        print('Wrong URL format, please try again!\n\n')
+  
 
 
 def one_category_books():
     category_page_url = input("Enter category page URL:")
-    category_name = scrape_category_page(category_page_url)
-    print(
-        f'All books info from the {category_name} category have been extracted!\n\n')
+    if (validators.url(category_page_url)):
+        category_name = scrape_category_page(category_page_url)
+        print(f'All books info from the {category_name} category have been extracted!\n\n')
+    else:
+        print('Wrong URL format, please try again!\n\n')
+
 
 
 def all_categories_books():
@@ -46,7 +53,7 @@ while(True):
     try:
         option = int(input('Enter your choice: '))
     except:
-        print('Wrong input. Please enter a number ...')
+        print('Wrong input. Please enter a number ...\n\n')
     # Check what choice was entered and act accordingly
     if option == 1:
         single_book()
@@ -55,7 +62,7 @@ while(True):
     elif option == 3:
         all_categories_books()
     elif option == 4:
-        print('Thank you for using Books To Scrape !')
+        print('Thank you for using Books To Scrape !\n\n')
         exit()
     else:
-        print('Invalid option. Please enter a number between 1 and 4.')
+        print('Invalid option. Please enter a number between 1 and 4.\n\n')
